05c2e07aa4c202d6d2e2a3ea84db69203c03ad42
Iago Ferreira <iagosfs2007.vr@gmail.com>
2025-04-06 13:46:37 -0300

    1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', function(){
     let header = document.querySelector('.header')
     header.classList.toggle('rolagem', window.scrollY >= 200)
-}) //rolagem que gera o menu fixo com fundo branco
+}) //rolagem que gera o menu fixo com bg branco
